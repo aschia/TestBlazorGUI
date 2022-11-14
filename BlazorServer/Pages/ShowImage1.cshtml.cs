004e3e680a836c65d6b6f3d005d1491ab051dfb4
aschia <ajs1173@utulsa.edu>
2022-11-14 13:25:07 -0600

    commit

---
+++ BlazorServer/Pages/ShowImage1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blazor.Pages
+{
+    public class ShowImage1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BlazorServer/Pages/UploadFile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blazor.Pages
+{
+    public class UploadFileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
